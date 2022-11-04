0114cd7fcc5b50ea53cf4cc6d7c35c749adc7ebd
kakon007 <kakon443@gmail.com>
2022-11-04 13:51:57 +0600

    Firebase google sign in package added

---
+++ lib/auth/view/sign_in_with_google.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignInWithGoole extends StatelessWidget {
+  const SignInWithGoole({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
